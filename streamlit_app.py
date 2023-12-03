6a03263d34900a7738c785f2518119ce95e32a26
Egri Mate <egrimate99@gmail.com>
2023-12-03 21:05:48 +0100

    valami

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import altair as alt
-
+from scipy.stats import norm
 
 # Model parameters
 S0 = 100  # initial stock price
@@ -74,7 +74,7 @@ $$ dS(t) = k(m-S(t)dt+\\sigma S(t)dW(t) $$
 # Slider for the mean-reversion parameter k
 k = st.slider('Mean-reversion intenzitás: k', min_value=0.01, max_value=10.0, value=1.5, step=0.01)
 m = st.slider('Mean-reversion középérték: m', min_value=0.01, max_value=200.0, value=100.0, step=0.01)
-sigma = st.slider('Volatilitás: sigma   ', min_value=0.01, max_value=0.4, value=0.3, step=0.01)
+sigma = st.slider('Volatilitás: sigma   ', min_value=0.01, max_value=0.8, value=0.3, step=0.01)
 
 # m and sigma are kept constant for this example
 # m = 100
@@ -173,7 +173,7 @@ $rS(t)dt+\\sigma S(t)d\\tilde{W}(t)$
 
 k_ = st.slider('Mean-reversion intenzitás: k (nem függ tőle)', min_value=0.01, max_value=10.0, value=1.5, step=0.01)
 m_ = st.slider('Mean-reversion középérték: m (nem függ tőle)', min_value=0.01, max_value=200.0, value=100.0, step=0.01)
-sigma_ = st.slider('Volatilitás: sigma', min_value=0.01, max_value=0.4, value=0.3, step=0.01)
+sigma_ = st.slider('Volatilitás: sigma', min_value=0.01, max_value=0.8, value=0.3, step=0.01)
 t, paths_b = generate_multiple_paths(k_, m_, sigma_, S0, r, T, N, num_paths, wiener_increments_all_paths,True)
 df_paths_b = pd.DataFrame(paths_b).T
 df_paths_b.index = t
@@ -209,3 +209,56 @@ st.altair_chart(chart_b_density, use_container_width=True)
 
 # Display the chart
 # chart_b_density
+
+def black_scholes(S, K, T, r, sigma, option_type="c"):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    if option_type == "c":
+        price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    else:
+        price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+    return price
+# from py_vollib_vectorized import vectorized_implied_volatility as implied_vol
+def implied_volatility(price, S, K, T, r, option_type):
+    precision = 0.00001
+    max_iterations = 100
+    sigma = 0.5  # initial guess
+    for i in range(max_iterations):
+        price_estimate = black_scholes(S, K, T, r, sigma, option_type)
+        vega = (1 / np.sqrt(2 * np.pi)) * S * np.sqrt(T) * np.exp(-0.5 * (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) ** 2 / (sigma ** 2 * T))
+        diff = price_estimate - price
+
+        if abs(diff) < precision:
+            return sigma
+
+        sigma -= diff / vega  # Newton-Raphson step
+
+        if sigma <= 0:  # ensure sigma stays positive
+            return np.nan
+
+    return np.nan  # return NaN if not converged
+def calculate_option_prices_and_ivs(paths, S0, K_values, T, r):
+    final_prices = np.array([path[-1] for path in paths])
+    puts = np.array([np.exp(-r*T) * np.mean(np.maximum(k - final_prices, 0)) for k in K_values])
+    calls = np.array([np.exp(-r*T) * np.mean(np.maximum(final_prices - k, 0)) for k in K_values])
+
+    put_ivs = np.array([implied_volatility(put_price, S0, k, T, r, 'p') for put_price, k in zip(puts, K_values)])
+    call_ivs = np.array([implied_volatility(call_price, S0, k, T, r, 'c') for call_price, k in zip(calls, K_values)])
+
+    return puts, calls, put_ivs, call_ivs
+
+K_values = np.linspace(S0 - 50, S0 + 100, 10)  # Ensure this is an array
+
+# Now calling the function with K_values
+puts, calls, put_ivs, call_ivs = calculate_option_prices_and_ivs(paths_b_density, S0, K_values, T, r)
+
+
+data = pd.DataFrame({
+    'Strike Price': K_values,
+    'Implied Volatility': call_ivs
+})
+c = alt.Chart(data).mark_line().encode(
+    x=alt.X('Strike Price', scale=alt.Scale(domain=[50, 200])),
+    y=alt.Y('Implied Volatility', scale=alt.Scale(domain=[0.0, 1.0])),
+).properties(width=600, height=400)
+st.altair_chart(c, use_container_width=True)
